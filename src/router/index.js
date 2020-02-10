b348b830b8543b058b74d87eea0c4cf3a8d8a566
unknown <vakanski@outlook.com>
2020-02-10 16:35:55 +0100

    build02102020-v4

--- src/router/index.js
+++ src/router/index.js
@@ -4,73 +4,75 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [{
-        path: "/",
-        name: "home",
-        component: Home,
-        meta: {
-            title: "Home Page - Example App",
-            metaTags: [{
-                    name: "description",
-                    content: "The home page of our example app."
-                },
-                {
-                    property: "og:description",
-                    content: "The home page of our example app."
-                }
-            ]
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+    meta: {
+      title: "Home Page - Example App",
+      metaTags: [
+        {
+          name: "description",
+          content: "The home page of our example app."
+        },
+        {
+          property: "og:description",
+          content: "The home page of our example app."
         }
-    },
-    {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/About.vue")
-    },
-    {
-        path: "/services",
-        name: "services",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Services.vue")
-    },
-    {
-        path: "/careers",
-        name: "careers",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Careers.vue")
-    },
-    {
-        path: "/contact",
-        name: "contact",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Contact.vue")
-    },
-
-    // for routes with named views, you have to define the `props` option for each named view:
-    {
-        path: "/career/:id",
-        props: true,
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/CareerSingle.vue")
+      ]
     }
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/services",
+    name: "services",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Services.vue")
+  },
+  {
+    path: "/careers",
+    name: "careers",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Careers.vue")
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue")
+  },
+
+  // for routes with named views, you have to define the `props` option for each named view:
+  {
+    path: "/career/:id",
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CareerSingle.vue")
+  }
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
 
 export default router;
